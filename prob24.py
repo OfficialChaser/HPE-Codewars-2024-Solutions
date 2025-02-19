f9cfc39fa98f32ebcf8283e32238f59c35d30874
Chase Maciel <112979903+OfficialChaser@users.noreply.github.com>
2025-02-19 15:09:56 -0800

    more practicing
    on that grind this break
    prep for codewars

--- prob24.py
+++ prob24.py
@@ -1,6 +1,38 @@
-## INCOMPLETE ##
 import os
-path = 'files\\problem24'
-files = os.listdir(path)
-for item in files:
-    print(item)
+
+end = False
+
+while not end:
+    genre = input()
+
+    if genre == "END":
+        end = True
+        continue
+    
+    library = []
+    for filename in os.listdir("student_datasets/files"):
+        if not filename.endswith(".mp3"):
+            continue
+
+        filename = filename.removesuffix(".mp3")
+
+        song, artist, ending = filename.strip().split(" - ")
+        song_genres = ending.split(", ")
+
+
+        matching_genre = False
+        for applicable_genre in song_genres:
+            if genre.lower() in applicable_genre.lower():
+                matching_genre = True
+                break
+
+        if not matching_genre:
+            continue
+        
+        printed_genre = genre.replace(" ", "")
+        song = song.replace(" ", "")
+
+        library.append(f"{printed_genre.upper()}_{song}_{artist}.mp3")
+    
+    for song in sorted(library):
+        print(song)
